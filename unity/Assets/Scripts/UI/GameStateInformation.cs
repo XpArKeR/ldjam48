664e85c64e7b1cc53b8ccd70b26522a37aa9b9d8
XpArKeR <xparker@gmx.net>
2021-04-25 00:16:38 +0200

    Updated HeaderBar

--- unity/Assets/Scripts/UI/GameStateInformation.cs
+++ unity/Assets/Scripts/UI/GameStateInformation.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
+
+using Assets.Scripts;
+
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameStateInformation : MonoBehaviour
 {
+    public Text turnText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    private void FixedUpdate()
+    {
+        this.turnText.text = Core.GameState.CurrentTurn.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
