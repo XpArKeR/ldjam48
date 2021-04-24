3da98e094396561d3bda927d7b3a91f5786b8a10
Salasam <Salasam@gmx.net>
2021-04-24 12:06:46 +0200

    planget generator

--- unity/Assets/Scripts/PlanetGenerator.cs
+++ unity/Assets/Scripts/PlanetGenerator.cs
@@ -5,34 +5,43 @@ using UnityEngine.UI;
 
 public class PlanetGenerator : MonoBehaviour
 {
-    public Sprite[] planets;
+    public Sprite[] planetLands;
+    public Sprite[] clouds;
 
     public Image planetBase;
     public Image planetLand;
     public Image planetClouds;
 
-    static List<Color> planetColors;
-
-    public void Start()
-    {
-        planetColors = new List<Color>()
-           {
-            new Color32(255, 255, 225, 255), 
-            new Color32(124, 47, 88, 255),
-            new Color32(148, 248, 225, 255) 
-    };
-    }
+    public Color[] planetColors;
+    public Color[] planetLandColors;
+    public Color[] planetCloudColors;
 
     public void GeneratePlanet()
     {
 
-        int baseColorIndex = Random.Range(0, planetColors.Count);
-        planetBase.color = planetColors[baseColorIndex];
+        planetBase.color = ChooseRandomColor(planetColors);
 
-        int baseLandIndex = Random.Range(0, planets.Length);
-        planetLand.sprite = planets[baseLandIndex];
+
+        planetLand.color = ChooseRandomColor(planetLandColors);
+        planetLand.sprite = ChooseRandomSprite(planetLands);
+
+
+        planetClouds.color = ChooseRandomColor(planetCloudColors);
+        planetClouds.sprite = ChooseRandomSprite(clouds);
 
     }
 
- 
+
+    private Color ChooseRandomColor(Color[] colors)
+    {
+        int baseLandIndex = Random.Range(0, colors.Length);
+        return colors[baseLandIndex];
+    }
+
+    private Sprite ChooseRandomSprite(Sprite[] sprites)
+    {
+        int baseLandIndex = Random.Range(0, sprites.Length);
+        return sprites[baseLandIndex];
+    }
+
 }
