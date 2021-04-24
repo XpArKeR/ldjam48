0ea60aea97fd4a553ae67012e18c92bf032c0ebb
Salasam <Salasam@gmx.net>
2021-04-24 11:31:47 +0200

    alpha planet generator

--- unity/Assets/Scripts/PlanetGenerator.cs
+++ unity/Assets/Scripts/PlanetGenerator.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlanetGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Sprite[] planets;
+
+    public Image planetBase;
+    public Image planetLand;
+    public Image planetClouds;
+
+    static List<Color> planetColors;
+
+    public void Start()
     {
-        
+        planetColors = new List<Color>()
+           {
+            new Color32(255, 255, 225, 255), 
+            new Color32(124, 47, 88, 255),
+            new Color32(148, 248, 225, 255) 
+    };
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GeneratePlanet()
     {
-        
+
+        int baseColorIndex = Random.Range(0, planetColors.Count);
+        planetBase.color = planetColors[baseColorIndex];
+
+        int baseLandIndex = Random.Range(0, planets.Length);
+        planetLand.sprite = planets[baseLandIndex];
+
     }
+
+ 
 }
