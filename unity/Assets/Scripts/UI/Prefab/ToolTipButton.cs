051bcc5f53e4fbf65ec78ba4a869721eede27082
XpArKeR <xparker@gmx.net>
2021-04-26 13:20:03 +0200

    Organized Scripts. Added Scan2 Audio

---
+++ unity/Assets/Scripts/UI/Prefab/ToolTipButton.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolTipButton : MonoBehaviour
+{
+    public GameObject ToolTip;
+        
+    public void OnPointerEnter()
+    {
+        this.ShowToolTip();
+    }
+
+    public void OnPointerExit()
+    {
+        this.HideToolTip();
+    }
+
+    public void OnButtonClick()
+    {
+        this.HideToolTip();
+    }
+
+    private void ShowToolTip()
+    {
+        this.ToolTip?.SetActive(true);
+    }
+    
+    private void HideToolTip()
+    {
+        this.ToolTip?.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
