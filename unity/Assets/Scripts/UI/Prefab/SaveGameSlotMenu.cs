72e4d1e3edd5b4ffa2a5c6a2de5736a86f86330b
XpArKeR <xparker@gmx.net>
2021-05-03 22:18:20 +0200

    Un-broke a lot of things

--- unity/Assets/Scripts/UI/Prefab/SaveGameSlotMenu.cs
+++ unity/Assets/Scripts/UI/Prefab/SaveGameSlotMenu.cs
@@ -29,11 +29,6 @@ public class SaveGameSlotMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-    }
-
-    private void FixedUpdate()
-    {
         if (this.Savegame != default)
         {
             this.Title.text = String.Format("Planets visited: {0}", this.Savegame.PlanetsVisited);
@@ -46,9 +41,23 @@ public class SaveGameSlotMenu : MonoBehaviour
         }
     }
 
+    private void FixedUpdate()
+    {
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
-
+        if (this.Savegame != default)
+        {
+            this.Title.text = String.Format("Planets visited: {0}", this.Savegame.PlanetsVisited);
+            this.SavedOn.text = this.Savegame.SavedOn.ToString("G");
+        }
+        else
+        {
+            this.Title.text = "";
+            this.SavedOn.text = "Empty";
+        }
     }
 }
